091b3fdd37027769e266283db6eb8d4584b45c5d
apcspp38 <graceydeng@gmail.com>
2018-05-24 18:34:31 +0000

    Updated

--- cdemo/iteration.c
+++ cdemo/iteration.c
@@ -5,11 +5,12 @@ int main()
 
 	int div = 10;
 	int i;
+	printf ("Numbers divisible by 10:\n");
 	for ( i = 1; i <= 100; i++ )
 
 	{
 		if (i % div == 0)
-			printf ("Numbers divisible by 10: %d, \n",i);
+			printf ("%d, \n",i);
 	}
 
 
