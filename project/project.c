54a68e5dbfba68b42022d8b6c5174d5ec2dcef5d
apcspp38 <38960628+apcspp38@users.noreply.github.com>
2018-06-10 21:19:08 -0700

    Update project.c

--- project/project.c
+++ project/project.c
@@ -2,25 +2,33 @@
 #include <string.h>
 
 void encrypt(int key, char sample[])
+{
+	
+	for (int i = 0; i < strlen(sample); i++)
+        {
+            
+        if ((sample[i] + key) < 104)
+        {
+            sample[i] = (sample[i] + key) % 26 + 78;
+        }
+        else
+        {
+           sample[i] = ((sample[i] + key) % 26 + 104);
+        }
+        
+        }
+    printf("%s", sample);
+   
 
-  {
-	for (int i = 0; i <= strlen(sample); i++)
-        {
-                sample[i] = sample[i] + key;
-		sample[i] = sample[i] % 26;
-        }
-        for (int i = 0; i <= strlen (sample); i++)
-        {
-           	printf("%s", sample);
-        }
-  }
+} 
 
 int main(int argc, char* argv[])
 {
-  int arg = 0;
-  while (arg < argc)
+   int arg = 1;
+   while (arg < argc)
   {
-    encrypt(2, argv[arg]);
+    encrypt(1, argv[arg]);
     arg++;
   }
-}
+   
+} 
