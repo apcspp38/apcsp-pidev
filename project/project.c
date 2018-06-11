5c1405e6574758ff43928b66b0fd03ea1b07b085
apcspp38 <38960628+apcspp38@users.noreply.github.com>
2018-06-10 21:43:23 -0700

    Update project.c

--- project/project.c
+++ project/project.c
@@ -5,18 +5,32 @@ void encrypt(int key, char sample[])
 {
 	
 	for (int i = 0; i < strlen(sample); i++)
-        {
-         if (((sample[i] + key) % 26) >= 19)
-        {
-            sample[i] = (sample[i] + key) % 26 + 78;
-        }
-        else
-        {
-           sample[i] = ((sample[i] + key) % 26 + 104);
+    {
+        if (sample[i]>='a' && sample[i] <= 'z')
+        {    
+            if (((sample[i] + key) % 26) >= 19)
+            {
+                sample[i] = (sample[i] + key) % 26 + 78;
+            }
+            else
+            {
+                sample[i] = ((sample[i] + key) % 26 + 104);
+            }
         }
         
-            
+        if (sample[i]>='A' && sample[i] <= 'Z')
+        {    
+            if (((sample[i] + key) % 26) >= 13)
+            {
+                sample[i] = (sample[i] + key) % 26 + 52;
+            }
+            else
+            {
+                sample[i] = ((sample[i] + key) % 26 + 78);
+            }
         }
+            
+    }
     printf("%s", sample);
    
 
@@ -27,8 +41,8 @@ int main(int argc, char* argv[])
    int arg = 1;
    while (arg < argc)
   {
-    encrypt(10, argv[arg]);
+    encrypt(2, argv[arg]);
     arg++;
   }
    
-} 
+}
