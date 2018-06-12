4344f1a9c381da46a81f59d5b375943a8e0b3c03
apcspp38 <38960628+apcspp38@users.noreply.github.com>
2018-06-12 12:02:08 -0700

    Update sort.c

--- project/sort.c
+++ project/sort.c
@@ -1,6 +1,31 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+   char sorted[argc][20], temp[20];
+   int i, j;
+   for (i = 0; i < argc; i++)
+      strcpy(sorted[i], argv[i]);
+
+   for (i = 1; i < argc; i++)
+   {
+      for (j = 1; j < argc; j++)
+      {
+         if (strcmp(sorted[j - 1], sorted[j]) > 0)
+         {
+            strcpy(temp, sorted[j - 1]);
+            strcpy(sorted[j - 1], sorted[j]);
+            strcpy(sorted[j], temp);
+         }
+      }
+   }
+
+   for (i = 1; i < argc; i++)
+      printf("%s\n", sorted[i]);
+ }
+
+
+
+
 	
